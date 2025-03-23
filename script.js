f3428de82721d40f6905697eea99aa217be2fac4
Margot Lafond <margot.lafond@etu.minesparis.psl.eu>
2025-03-23 11:30:05 +0100

    Calculatrice fonctionne sans les chiffres à virgule

--- script.js
+++ script.js
@@ -12,12 +12,20 @@
 
 let calculator = document.querySelector('.calculator')
 let keys = calculator.querySelector('.calculator__keys')
+let nb1 = 0
+let nb2 = 0
+let preoperation = true
+let op = ""
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         for (let i=0; i<=9; i++) {
             if (e.target.textContent === `${i}`) {
-                if (document.getElementById("screen").innerHTML === '0') {
+                if (document.getElementById("screen").innerHTML === '0' 
+                | document.getElementById("screen").innerHTML === '×'
+                | document.getElementById("screen").innerHTML === '-'
+                | document.getElementById("screen").innerHTML === '+'
+                | document.getElementById("screen").innerHTML === '÷') {
                     document.getElementById("screen").innerHTML = i
                 }
                 else {
@@ -25,9 +33,33 @@ keys.addEventListener('click', e => {
                 }
                 // le += i met les chiffres à la suite
                 console.log(i)
+                if (preoperation) {nb1 = Number(document.getElementById("screen").innerHTML)}
+                else {nb2 = Number(document.getElementById("screen").innerHTML)}
             }
             if (e.target.textContent === "AC") {
                 document.getElementById("screen").innerHTML = 0
+                nb1 = 0
+                nb2 = 0
+                preoperation = true
+            }
+            if (e.target.classList.contains("key--operator")) {
+                preoperation = false
+                op = e.target.getAttribute("data-action")
+                document.getElementById("screen").innerHTML = e.target.textContent
+                console.log(e.target.textContent)
+            }
+            if (e.target.classList.contains("key--equal")) {
+                if (!preoperation) {
+                    let res = 0
+                    if (op === "add") {res = nb1+nb2}
+                    if (op === "subtract") {res = nb1-nb2}
+                    if (op === "multiply") {res = nb1*nb2}
+                    if (op === "divide") {res = nb1/nb2}
+                    document.getElementById("screen").innerHTML = res
+                    nb1 = res
+                    preoperation = true
+
+                }
             }
         }
     }
